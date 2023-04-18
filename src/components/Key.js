8fdd55beef26f26b29abf3684fbbf1e78822d3ef
Patrick Speranza <patrick.m.speranza@gmail.com>
2023-04-17 19:53:37 -0500

    Refactor play note function in Key.js

--- src/components/Key.js
+++ src/components/Key.js
@@ -2,18 +2,15 @@ import "./Key.css"
 
 const Key = ({ id, className, stickerClassName, stickerText }) => {
   const playNote = (e) => {
-    const number = e.target.id
+    console.log(e)
+    const number = e.target.id || e.target.parentNode.id
     const noteAudio = new Audio(`./pianoSamples/key${number}.mp3`)
     noteAudio.play()
   }
-  const playNoteSticker = (e) => {
-    const number = e.target.parentNode.id
-    const noteAudio = new Audio(`./pianoSamples/key${number}.mp3`)
-    noteAudio.play()
-  }
+
   return (
     <div id={id} className={className} onMouseDown={playNote}>
-      <div className={stickerClassName} onMouseDown={playNoteSticker}>
+      <div className={stickerClassName} onMouseDown={() => {}}>
         {stickerText}
       </div>
     </div>
